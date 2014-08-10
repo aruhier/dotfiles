ee2295a495cf7dd6344794523a122249d39970af
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-08-10 22:22:00 +0200

    Add favorite branch in automerge

--- automerge.sh
+++ automerge.sh
@@ -7,3 +7,7 @@ git merge --no-edit master
 # Merge tour-anthony and desktop in tour-anthony
 git checkout tour-anthony
 git merge --no-edit desktop
+
+if [ -f favorite ]
+    then git checkout $(cat favorite)
+fi
