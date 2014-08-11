b83bf8b20b6f518481ea8f1b736f494222d4d530
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-08-11 16:25:56 +0200

    Use rebase -m in automerge

--- automerge.sh
+++ automerge.sh
@@ -2,11 +2,11 @@
 
 # Merge master and desktop in desktop
 git checkout desktop
-git rebase master
+git rebase -m master
 
 # Merge tour-anthony and desktop in tour-anthony
 git checkout tour-anthony
-git rebase desktop
+git rebase -m desktop
 
 if [ -f favorite ]
     then git checkout $(cat favorite)
