58675bddc3771a4d593aa2f10538d81e11ca2b52
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-08-12 00:16:28 +0200

    Automerge stops if any conflict

--- automerge.sh
+++ automerge.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# Auto-merge branch between each other
+#
+
+# checkout_and_rebase branch_to_checkout branch_to_merge_in
+# Cancel if any error
+function checkout_and_rebase()
+{
+    git checkout "$1" || exit 1
+    git rebase -m "$2" || exit 1
+}
 
 # Merge master and desktop in desktop
-git checkout desktop
-git rebase -m master
+checkout_and_rebase "desktop" "master"
 
 # Merge tour-anthony and desktop in tour-anthony
-git checkout tour-anthony
-git rebase -m desktop
+checkout_and_rebase "tour-anthony" "desktop"
 
 if [ -f favorite ]
-    then git checkout $(cat favorite)
+    then git checkout $(cat favorite) || exit 1
 fi
