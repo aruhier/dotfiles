4cd43f8e7c635594bc286a54dc5a89cf39ce63ac
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-08-12 00:16:28 +0200

    Automerge stops if any conflict

--- automerge.sh
+++ automerge.sh
@@ -11,19 +11,10 @@ function checkout_and_rebase()
 }
 
 # Merge master and desktop in desktop
-<<<<<<< master
 checkout_and_rebase "desktop" "master"
 
 # Merge tour-anthony and desktop in tour-anthony
 checkout_and_rebase "tour-anthony" "desktop"
-=======
-git checkout desktop
-git rebase -m master
-
-# Merge tour-anthony and desktop in tour-anthony
-git checkout tour-anthony
-git rebase -m desktop
->>>>>>> HEAD~9
 
 # Merge pc-trinaps and desktop in pc-trinaps
 checkout_and_rebase "pc-trinaps" "desktop"
