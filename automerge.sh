d23ee84cb94bbbd5c849e2ba77045b497015cb10
Anthony Ruhier <anthony.ruhier@gmail.com>
2014-08-23 01:16:23 +0200

    Add continue for automerge
    Store the current branch name to merge before merging to, to continue
    directly to this branch in case of conflict

--- automerge.sh
+++ automerge.sh
@@ -6,19 +6,36 @@
 # Cancel if any error
 function checkout_and_rebase()
 {
+    echo "$1" > .current_merge
     git checkout "$1" || exit 1
     git pull --rebase || exit 1
     git merge --no-edit "$2" || exit 1
+    rm .current_merge
+}
+
+function pre_checkout_and_rebase()
+{
+    if [ ! -f .current_merge ]
+        then checkout_and_rebase $1 $2
+        return
+    fi
+
+    stopped_merge="$(cat .current_merge)"
+    if [ "$1" == "$stopped_merge" ]
+        then checkout_and_rebase $1 $2
+    else
+        return
+    fi
 }
 
 # Merge master and desktop in desktop
-checkout_and_rebase "desktop" "master"
+pre_checkout_and_rebase "desktop" "master"
 
 # Merge tour-anthony and desktop in tour-anthony
-checkout_and_rebase "tour-anthony" "desktop"
+pre_checkout_and_rebase "tour-anthony" "desktop"
 
 # Merge pc-trinaps and desktop in pc-trinaps
-checkout_and_rebase "pc-trinaps" "desktop"
+pre_checkout_and_rebase "pc-trinaps" "desktop"
 
 if [ -f favorite ]
     then git checkout $(cat favorite) || exit 1
