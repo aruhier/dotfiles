1d0977e286e0e084d4a2dd1eb1806c1c13ef4df5
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-09-09 00:53:37 +0200

    qtile: load nvidia config

--- qtile/qtile/autostart.sh
+++ qtile/qtile/autostart.sh
@@ -4,6 +4,7 @@ light-locker --no-lock-on-suspend &
 xset -dpms &
 xset s off &
 
+nvidia-settings --load-config-only &
 compton -cG --backend glx --unredir-if-possible &
 feh --bg-fill ~/.config/qtile/wallpaper.jpg &
 sleep 1
