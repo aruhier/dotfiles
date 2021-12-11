52e1867f0ecacf145471d99f0836d28be3c72d2d
Anthony Ruhier <aruhier@mailbox.org>
2021-12-11 16:43:16 +0100

    nvim: move config to lua

--- nvim/post_install.sh
+++ nvim/post_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f ~/.config/nvim/init.vim -a -f ~/.config/nvim/init.lua ]
+  then rm ~/.config/nvim/init.vim
+fi
+
 nvim --headless "+PlugUpdate" "+qall" 2> /dev/null
 nvim --headless "+PlugClean!" "+qall" 2> /dev/null
 # Force to remove all lsp clients to update them.
