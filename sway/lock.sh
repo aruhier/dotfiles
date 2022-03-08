33127b9f9245b8844c2c81b9e296f7f273171249
Anthony Ruhier <aruhier@mailbox.org>
2022-03-08 01:25:09 +0100

    sway: avoid weird dpms conflict

--- sway/lock.sh
+++ sway/lock.sh
@@ -2,6 +2,7 @@
 # Times the screen off and puts it to background
 swayidle \
     timeout 5 'swaymsg "output * dpms off"' \
+    after-resume 'swaymsg "output * dpms on"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
 swaylock -c 000000
