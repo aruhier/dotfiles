d781ad010cab868e08652ee8aaa25531e5f5c462
Anthony Ruhier <aruhier@mailbox.org>
2022-12-26 08:22:15 +0100

    sway: remove sleep at start

--- sway/start.sh
+++ sway/start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 swaync&
+lxqt-policykit-agent&
 waybar&
 GTK_THEME=Adwaita:dark /usr/lib/xfce4/notifyd/xfce4-notifyd&
 nm-applet --indicator&
-sleep 1
-lxqt-policykit-agent&
 gammastep-indicator&
 kdeconnect-indicator&
 nextcloud --background&
