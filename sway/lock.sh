a34bf356813850a42441ec100e9c75ddddc7671a
Anthony Ruhier <aruhier@mailbox.org>
2022-12-29 12:39:11 +0100

    sway: fix lock issues

--- sway/lock.sh
+++ sway/lock.sh
@@ -2,8 +2,8 @@
 # Times the screen off and puts it to background
 swayidle \
     timeout 5 'swaymsg "output * dpms off"' \
-    after-resume 'swaymsg "output * dpms on"' \
-    resume 'swaymsg "output * dpms on"' &
+    resume 'swaymsg "output * dpms on"' \
+    after-resume 'swaymsg "output * dpms on"'&
 # Locks the screen immediately
 swaylock -c 000000
 # Kills last background task so idle timer doesn't keep running
