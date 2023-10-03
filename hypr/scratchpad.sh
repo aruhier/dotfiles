6128fd30eca3dae6e619443657456cb64872920e
Anthony Ruhier <aruhier@mailbox.org>
2023-10-03 12:31:15 +0200

    hyprland: simplify scratchpad script
    Now hyprland shows automatically the scratchpad when opening a window on
    it.

--- hypr/scratchpad.sh
+++ hypr/scratchpad.sh
@@ -24,21 +24,6 @@ done
 hyprctl workspaces | grep special:$name > /dev/null
 if [[ $? > 0 ]]
     then hyprctl dispatch exec "[workspace special:${name};noanim]" $prog > /dev/null
-    counter=0
-    while true
-    do
-        hyprctl workspaces | grep special:$name > /dev/null
-        if [[ $? == 0 ]]
-            then break
-        fi
-        if (( $counter > 50 ))
-            then echo "Cannot find the scratchpad"
-            exit 2
-        fi
-        sleep 0.2
-        counter=$((counter+1))
-    done
-    hyprctl dispatch workspace special:$name > /dev/null
 else
     hyprctl dispatch togglespecialworkspace $name > /dev/null
 fi
