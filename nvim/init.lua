bfcfba862b368a467699fe6a3c4fca4529369527
Anthony Ruhier <aruhier@mailbox.org>
2024-08-01 17:53:07 +0200

    nvim: fix nvim-cmp cmdline behavior

--- nvim/init.lua
+++ nvim/init.lua
@@ -388,6 +388,7 @@ local function setupNvimCMP()
 
   -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = {
       { name = 'buffer' }
     }
@@ -395,11 +396,13 @@ local function setupNvimCMP()
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
       { name = 'path' }
     }, {
       { name = 'cmdline' }
-    })
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
   })
 end
 setupNvimCMP()
