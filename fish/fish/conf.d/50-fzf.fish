d92c33d1f1d081f81262886b5b01ff6ac9afb6cd
Anthony Ruhier <aruhier@mailbox.org>
2025-04-01 01:49:53 +0200

    fish: add potential path for fzf

--- fish/fish/conf.d/50-fzf.fish
+++ fish/fish/conf.d/50-fzf.fish
@@ -5,6 +5,8 @@ if test -f ~/.fzf.zsh
     source ~/.fzf.fish
 else if test -f /usr/share/fzf/key-bindings.fish
     source /usr/share/fzf/key-bindings.fish
+else if test -f /usr/share/fzf/shell/key-bindings.fish
+    source /usr/share/fzf/shell/key-bindings.fish
 else if test -f /usr/share/doc/fzf/examples/key-bindings.fish
     source /usr/share/doc/fzf/examples/key-bindings.fish
 end
