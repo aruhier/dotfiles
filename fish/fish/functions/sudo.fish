84934473a11b8bcf5d25e6167aa7c8066de70148
Anthony Ruhier <aruhier@mailbox.org>
2025-04-03 00:55:52 +0200

    fish: fix options in the sudo function

--- fish/fish/functions/sudo.fish
+++ fish/fish/functions/sudo.fish
@@ -1,24 +1,24 @@
 function sudo --wraps='sudo'
     set -l options
-    set -l args
 
     # Separate options (starting with -) from the rest
     for arg in $argv
         if string match -qr '^-' -- $arg
             set options $options $arg
+            set -e argv[1]
         else
-            set args $args $arg
+            break
         end
     end
 
-    set -l func (functions --no-details $args[1])
+    set -l func (functions --no-details $argv[1])
     # If the first non-option argument is an alias.
     if string match -q "*--description 'alias *" -- $func[1]
         # Read the function definition and get the first non-comment line
         set -l alias_content (echo $func[2..] | sed -E 's/\s*(command)?\s*(.*)\s+\$argv\s*(end)?/\2/')
-        set -e args[1]
-        set args (string split ' ' $alias_content) $args
+        set -e argv[1]
+        set argv (string split ' ' $alias_content) $argv
     end
 
-    command sudo $options $args
+    command sudo $options $argv
 end
