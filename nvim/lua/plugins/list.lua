e6eed20d9290d4a2d70e3cce84dbd45c4fa373a6
Anthony Ruhier <aruhier@mailbox.org>
2025-09-18 17:54:32 +0200

    nvim: move to lazy.nvim

--- nvim/lua/plugins/list.lua
+++ nvim/lua/plugins/list.lua
@@ -3,43 +3,63 @@
     ----------------------------------------------------------------
 
 require("plugins.settings")
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+		},
+		config = function()
+			PluginSetupTelescope()
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	{ "tpope/vim-surround" },
+	{
+		"sainnhe/everforest",
+		config = function()
+			PluginSetupEverforestTheme()
+		end,
+	},
+	{ "chaoren/vim-wordmotion" },
+	{ "tpope/vim-sleuth" },
+	{
+		"chentoast/marks.nvim",
+		config = function()
+			require("marks").setup()
+		end,
+	},
+	{
+		"shellRaining/hlchunk.nvim",
+		config = function()
+			PluginSetupHLChunk()
+		end,
+	},
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    requires = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
-    config = function() PluginSetupTelescope() end
-  }
-  use {
-    'windwp/nvim-autopairs',
-    config = function() require("nvim-autopairs").setup {} end
-  }
-  use 'tpope/vim-surround'
-  use {'sainnhe/everforest', config = function() PluginSetupEverforestTheme() end }
-  use 'chaoren/vim-wordmotion'
-  use 'tpope/vim-sleuth'
-  use {
-    'chentoast/marks.nvim',
-    config = function() require('marks').setup() end
-  }
-  use {'shellRaining/hlchunk.nvim', config = function() PluginSetupHLChunk() end }
+    ---- Status line
+	{
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+		config = function()
+			PluginSetupLuaLine()
+		end,
+	},
 
-  ---- Status line
-  use {'nvim-lualine/lualine.nvim', requires={'kyazdani42/nvim-web-devicons'}, config = function() PluginSetupLuaLine() end }
-
-  -- Multiple cursors, with ctrl+n
-  use {'mg979/vim-visual-multi', branch = 'master'}
-end)
+    -- Multiple cursors, with ctrl+n
+	{ "mg979/vim-visual-multi", branch = "master" },
+}
--- nvim/post_install.sh
+++ nvim/post_install.sh
@@ -6,9 +6,10 @@ if [ -f ~/.config/nvim/init.vim -a -f ~/.config/nvim/init.lua ]
   then rm ~/.config/nvim/init.vim
 fi
 
-if [ ! -e ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]
-  then git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-  echo "!!! WARNING !!! nvim moved to Packer, please remove your vim-plug plugins"
+
+if [ -e ~/.local/share/nvim/site/pack ]; then
+  echo "!!! WARNING !!! nvim moved to Packer"
+  rm -rf ~/.local/share/nvim/site/pack
 fi
 
-nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync" 2> /dev/null
+nvim --headless "+Lazy! sync" +qa 2> /dev/null
