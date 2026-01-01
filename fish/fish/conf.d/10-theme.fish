c5ffab81e24bd4e6abb9e08e7c1f2f3021121461
Anthony Ruhier <aruhier@mailbox.org>
2026-01-01 18:01:30 +0100

    fish: update to 4.3

---
+++ fish/fish/conf.d/10-theme.fish
@@ -0,0 +1,46 @@
+status is-interactive
+or exit 0
+
+set -g fish_color_autosuggestion brblack
+set -g fish_color_cancel -r
+set -g fish_color_cmd_duration 909090
+set -g fish_color_command normal
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end green
+set -g fish_color_error brred
+set -g fish_color_escape brcyan
+set -g fish_color_gray 5D6B66
+set -g fish_color_history_current --bold
+set -g fish_color_host green
+set -g fish_color_host_remote yellow
+set -g fish_color_keyword normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator brcyan
+set -g fish_color_option cyan
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection cyan --bold
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_user cyan
+set -g fish_color_user_root red
+set -g fish_color_valid_path --underline
+set -g fish_color_vcs_branch 909090
+set -g fish_pager_color_background
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description yellow -i
+set -g fish_pager_color_prefix normal --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_secondary_background
+set -g fish_pager_color_secondary_completion
+set -g fish_pager_color_secondary_description
+set -g fish_pager_color_secondary_prefix
+set -g fish_pager_color_selected_background -r
+set -g fish_pager_color_selected_completion
+set -g fish_pager_color_selected_description
+set -g fish_pager_color_selected_prefix
+set -g fish_pager_color_selected_progress --bold 6e6e6e
