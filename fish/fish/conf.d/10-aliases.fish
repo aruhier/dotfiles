8b7dbccd0155f423b0a02a53c41d019a7bf3c58e
Anthony Ruhier <aruhier@mailbox.org>
2026-07-26 18:10:30 +0200

    fish: add alias dig for doggo
    Dig is now packed with the full bind package on gentoo, doggo is
    lighter.

--- fish/fish/conf.d/10-aliases.fish
+++ fish/fish/conf.d/10-aliases.fish
@@ -1,12 +1,16 @@
 # Edit functions/_myaliases.fish for the _myaliases function that generates all
 # the aliases.
 
-# Dynamic alias, cannot be saved in a file.
+# Dynamic aliases, cannot be saved in a file.
 if type -q nvim
     alias v='nvim'
     alias vim='nvim'
 else
     alias v='vim'
+end
+
+if not command -q dig
+    alias dig='doggo'
 end
 
 abbr -a majabbr 'systemd-run -t -p CPUWeight=20 -p CPUQuota=3000% --user -- sudo emerge -uDU --ask --keep-going --newrepo --with-bdeps=y -j 5 --load-average=20 @world && sudo emerge --depclean && sudo eclean-dist --deep
